26f13574e3874e7849d542d9b486ec277a516ee1
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-25 15:56:15 +0530

    application.properties updated

--- src/main/java/lk/ijse/gdse/ticketManagement/controller/BusController.java
+++ src/main/java/lk/ijse/gdse/ticketManagement/controller/BusController.java
@@ -1,9 +1,10 @@
 package lk.ijse.gdse.ticketManagement.controller;
 
-import lk.ijse.gdse.ticketManagement.service.BusService;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lk.ijse.gdse.ticketManagement.service.BusService;
+import lombok.AllArgsConstructor;
 
 /**
  * @author Praboda Thathsarani
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class BusController {
     final private BusService busService;
-
+    
+    
 
 }
