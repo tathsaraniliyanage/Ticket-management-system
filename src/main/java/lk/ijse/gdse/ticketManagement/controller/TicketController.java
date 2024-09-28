18bfb1bd87d486269fc5ba4a73391e8a2b8e2eeb
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-28 21:30:31 +0530

    Ticket controller updated

--- src/main/java/lk/ijse/gdse/ticketManagement/controller/TicketController.java
+++ src/main/java/lk/ijse/gdse/ticketManagement/controller/TicketController.java
@@ -1,7 +1,12 @@
 package lk.ijse.gdse.ticketManagement.controller;
 
+import lk.ijse.gdse.ticketManagement.dto.TicketDTO;
 import lk.ijse.gdse.ticketManagement.service.TicketService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class TicketController {
     final private TicketService ticketService;
+
+    @PostMapping
+    public ResponseEntity SaveTicket(@RequestBody TicketDTO ticketDTO){
+        return ResponseEntity.ok("hello");
+    }
 }
+
